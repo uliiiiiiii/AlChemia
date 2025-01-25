1d09b8848ba748917a6039b32d17df44304c99f7
Uli <ulilike161@gmail.com>
2025-01-25 23:39:22 +0200

    deleted unused code

--- backend/main.py
+++ backend/main.py
@@ -9,28 +9,6 @@ from fastapi.responses import JSONResponse
 # Initialize FastAPI app
 app = FastAPI()
 
-# Load the pre-trained model
-model = load_chemml_model("../python_model/saved_MLP_chemml_model.json")
-
-x_scaler = StandardScaler()
-y_scaler = StandardScaler()
-
-def convert_smiles_to_features(smiles: str):
-    mol = Chem.MolFromSmiles(smiles)
-    if not mol:
-        raise ValueError("Invalid SMILES string.")
-    # Replace this with your actual feature extraction logic
-    features = []
-    return features
-
-def fetch_actual_density(smiles: str) -> float:
-    """
-    Placeholder for fetching the actual density from a database or API.
-    Replace this with your logic to fetch the actual density.
-    """
-    # Example: Mocked actual density
-    return 1.23  # Replace with actual density fetching logic
-
 def generate_3d_coordinates(smiles: str):
     mol = Chem.MolFromSmiles(smiles)
     if not mol:
@@ -62,21 +40,12 @@ def generate_3d_coordinates(smiles: str):
 @app.get("/api/fetchMolecule")
 async def predict_density(smiles: str):
     try:
-        # Feature extraction and prediction
-        features = convert_smiles_to_features(smiles)
-        scaled_features = x_scaler.transform([features])
-        scaled_prediction = model.predict(scaled_features)
-        predicted_density = y_scaler.inverse_transform(scaled_prediction)[0][0]
-
-        # Fetch actual density
-        # actual_density = fetch_actual_density(smiles)
-
         # Generate 3D coordinates
         molecule_data = generate_3d_coordinates(smiles)
 
         return {
             "smiles": smiles,
-            "predicted_density": predicted_density,
+            # "predicted_density": predicted_density,
             "actual_density": 69,
             "molecule_data": molecule_data
         }
